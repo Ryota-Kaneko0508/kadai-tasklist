15a7c16038f927041fd542706bfe7777695b4184
ryota.kaneko <kane2508ryo@gmail.com>
2021-05-10 11:23:10 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
   root to: 'tasks#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :tasks
+  
   get 'signup', to: 'users#new'
-  resources :users, only: [:create]
+  resources :users, only: [:index, :show, :create]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
