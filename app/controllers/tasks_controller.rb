aea56baa8ddec239bd59bb8779996e92a23ca66f
ryota.kaneko <kane2508ryo@gmail.com>
2021-05-11 09:11:33 +0000

    one-to-many relationship.3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show, :create, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :correct_user, only: [:destroy]
+  
+  
   
   def index
     if logged_in?
-      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      @tasks = current_user.tasks
     end
   end
   
@@ -16,7 +19,6 @@ class TasksController < ApplicationController
   end
   
   def create
-    #@task = Task.new(task_params)
     @task = current_user.tasks.build(task_params)
     
     if @task.save
@@ -52,11 +54,17 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
   
   def task_params
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless  @task
+      redirect_to root_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :tasks
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:create]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
